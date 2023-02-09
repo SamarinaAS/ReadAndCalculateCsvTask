cfab9192359a867a6e7380bce68dff58903b62d2
SamarinaAS <a.samarina1997@yandex.ru>
2023-02-09 17:47:22 +0300

    Open file

--- open_csv_and_calculate.cpp
+++ open_csv_and_calculate.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
 
 void readAndCalculateCsv(std::string fileName){
-    std::cout<<fileName<<std::endl;
+    std::string suffix = ".csv";
+    if (suffix.length() > fileName.length() || fileName.compare(fileName.length() - suffix.length(), suffix.length(), suffix) !=0){
+        throw "Invalid input. The file must be in csv format";
+    }
+    std::ifstream file (fileName);
+    if (file.is_open()){
+        std::cout<<"File is open"<<std::endl;
+    }
+    else{
+        throw "File could not open";
+}
     return;
 }
 
 int main (int argc, char* argv[]){
     if (argc == 1 || argc > 2){
-        throw "Invalid argument. The name of the csv file must be passed as the first argument on the command line.";
+        throw "Invalid input. The name of the csv file must be passed as the first argument on the command line.";
     }
     std::string fileName = argv[1];
     readAndCalculateCsv(fileName);
