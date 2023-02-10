8c7116730eee8380cabf3bb6c4a9064785d8ecf6
SamarinaAS <a.samarina1997@yandex.ru>
2023-02-10 14:34:42 +0300

    Change parsing formula: began to use regex

--- open_csv_and_calculate.cpp
+++ open_csv_and_calculate.cpp
@@ -6,10 +6,37 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <regex>
-bool isFormulas(std::string cell){
-    const std::regex r("\\=[a-zA-Z]+\\d+(\\+|\\-|\\*|\\/)[a-zA-Z]+\\d+");    
+
+bool isFormula(std::string cell){
+    const std::regex r("\\=([a-zA-Z]+)(\\d+)(\\+|\\-|\\*|\\/)([a-zA-Z]+)(\\d+)");
+
     return std::regex_match(cell, r);
 }
+
+std::vector<std::pair<std::string, std::string>> parseArguments(std::string cell){
+    const std::regex r("([a-zA-Z]+)(\\d+)");
+    const std::vector<std::smatch> matches{
+        std::sregex_iterator{cbegin(cell), cend(cell), r},
+        std::sregex_iterator{}
+    };
+    std::pair <std::string, std::string> p1(matches[0].str(1), matches[0].str(2));
+    std::pair <std::string, std::string> p2(matches[1].str(1), matches[1].str(2));
+    std::vector<std::pair<std::string, std::string>> arguments;
+    arguments.push_back(p1);
+    arguments.push_back(p2);
+    
+  return arguments;
+}
+
+
+char parseOperation(std::string cell){
+    const std::regex r("(\\+|\\-|\\*|\\/)");
+    std::smatch match;
+    regex_search (cell, match, r);
+    char operation = match.str(1)[0];
+  return operation;
+}
+
 
 bool isNumber(std::string cell){
     const std::regex r("\\d+");    
@@ -28,11 +55,12 @@ void calculate(std::vector < std::vector < std::string >> & tableData) {
         for (int j = 0; j < tableData[i].size(); j++) {
             std::string tempCell = tableData[i][j];
             
-            if (isFormulas(tempCell)) {
+            if (isFormula(tempCell)) {
+                std::vector<std::pair<std::string, std::string>> arguments = parseArguments(tempCell);
+                char operation = parseOperation(tempCell);
                 int k = 1;
                 std::string arg1Letter, arg2Letter, arg1Digit, arg2Digit;
-                char operation;
-                while (isalpha(tempCell[k])) {
+                /*while (isalpha(tempCell[k])) {
                     arg1Letter.push_back(tempCell[k]);
                     k++;
                 }
@@ -49,31 +77,32 @@ void calculate(std::vector < std::vector < std::string >> & tableData) {
                 while (isdigit(tempCell[k])) {
                     arg2Digit.push_back(tempCell[k]);
                     k++;
-                }
+                }*/
                 int a, b;
                 int aRow = -1, bRow = -1, aColumn = -1, bColumn = -1;
+                
                 for (int i = 0; i < tableData.size(); i++) {
-                    if (arg1Digit == tableData[i][0]) {
+                    if (arguments[0].second == tableData[i][0]) {
                         aRow = i;
                         break;
                     }
                 }
 
                 for (int i = 0; i < tableData.size(); i++) {
-                    if (arg2Digit == tableData[i][0]) {
+                    if (arguments[1].second == tableData[i][0]) {
                         bRow = i;
                         break;
                     }
                 }
                 for (int j = 0; j < tableData[0].size(); j++) {
-                    if (arg1Letter == tableData[0][j]) {
+                    if (arguments[0].first == tableData[0][j]) {
                         aColumn = j;
                         break;
                     }
                 }
 
                 for (int j = 0; j < tableData[0].size(); j++) {
-                    if (arg2Letter == tableData[0][j]) {
+                if (arguments[1].first == tableData[0][j]) {
                         bColumn = j;
                         break;
                     }
@@ -126,12 +155,7 @@ void readAndCalculateCsv(std::string fileName) {
             tableData.push_back(vectorLine);
             vectorLine.clear();
         }
-        /*for (int i = 0; i < tableData.size(); i++) {
-            for (int j = 0; j < tableData[i].size(); j++) {
-                std::cout << tableData[i][j] << ",";
-            }
-            std::cout << std::endl;
-        }*/
+
     } else {
         throw "File could not open";
     }
@@ -155,7 +179,13 @@ int main(int argc, char * argv[]) {
     }
     std::string fileName = argv[1];
     readAndCalculateCsv(fileName);
-    std::string tempStr = "=A1^B1";
-    std::cout<<isFormulas(tempStr)<<std::endl;
+    //std::string tempStr = "=A1+Cell1";
+    //std::cout<<isFormula(tempStr)<<std::endl;
+    //const std::vector<std::smatch> matches = parseFolmula(tempStr);
+    //std::cout<<matches[0].str(1)<<" "<<matches[0].str(2)<<std::endl;
+    //std::cout<<matches[1].str(1)<<" "<<matches[1].str(2)<<std::endl;
+    //std::cout<<matches[0].first<<" "<<matches[0].second<<std::endl;
+    //std::cout<<matches[1].first<<" "<<matches[1].second<<std::endl;
+    //parseOperation(tempStr);
     return 0;
 }
