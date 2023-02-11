92b5f0f09225ea7e9247c09544df7a888a77f8ba
SamarinaAS <a.samarina1997@yandex.ru>
2023-02-11 16:11:22 +0300

    structure for formula cell added, some code blocks separated into separate functions

--- open_csv_and_calculate.cpp
+++ open_csv_and_calculate.cpp
@@ -1,56 +1,59 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <sstream>
 #include <ctype.h>
 #include <stdlib.h>
-#include <regex>
 
-struct FormulaCell{
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+struct FormulaCell {
     std::pair<int, int> resultCelliIndices;
     std::pair<std::string, std::string> argument1;
     std::pair<std::string, std::string> argument2;
     char operation;
-    
-    FormulaCell(std::pair<int, int> resultCelliIndices, std::pair<std::string, std::string> argument1,
-    std::pair<std::string, std::string> argument2, char operation){
-    this->resultCelliIndices = resultCelliIndices;
-    this->argument1 = argument1;
-    this->argument2 = argument2;
-    this->operation = operation;
+
+    FormulaCell(std::pair<int, int> resultCelliIndices,
+                std::pair<std::string, std::string> argument1,
+                std::pair<std::string, std::string> argument2, char operation) {
+        this->resultCelliIndices = resultCelliIndices;
+        this->argument1 = argument1;
+        this->argument2 = argument2;
+        this->operation = operation;
+    }
+
+    FormulaCell(std::pair<std::string, std::string> argument1,
+                std::pair<std::string, std::string> argument2, char operation) {
+        this->resultCelliIndices = std::make_pair(-1, -1);
+        this->argument1 = argument1;
+        this->argument2 = argument2;
+        this->operation = operation;
     }
 };
 
 bool isFormula(std::string cell) {
-    const std::regex r("\\=([a-zA-Z]+)(\\d+)(\\+|\\-|\\*|\\/)([a-zA-Z]+)(\\d+)");
+    const std::regex r(
+        "\\=([a-zA-Z]+)(\\d+)(\\+|\\-|\\*|\\/)([a-zA-Z]+)(\\d+)");
 
     return std::regex_match(cell, r);
 }
 
-std::vector < std::pair < std::string, std::string >> parseArguments(std::string cell) {
-    const std::regex r("([a-zA-Z]+)(\\d+)");
-    const std::vector < std::smatch > matches {
-        std::sregex_iterator {
-                cbegin(cell), cend(cell), r
-            },
-            std::sregex_iterator {}
-    };
-    std::pair < std::string, std::string > p1(matches[0].str(1), matches[0].str(2));
-    std::pair < std::string, std::string > p2(matches[1].str(1), matches[1].str(2));
-    std::vector < std::pair < std::string, std::string >> arguments;
-    arguments.push_back(p1);
-    arguments.push_back(p2);
+FormulaCell parseFormula(std::string cell) {
+    const std::regex arg("([a-zA-Z]+)(\\d+)");
+    const std::vector<std::smatch> matches{
+        std::sregex_iterator{cbegin(cell), cend(cell), arg},
+        std::sregex_iterator{}};
 
-    return arguments;
-}
-
-char parseOperation(std::string cell) {
-    const std::regex r("(\\+|\\-|\\*|\\/)");
+    const std::regex oper("(\\+|\\-|\\*|\\/)");
     std::smatch match;
-    regex_search(cell, match, r);
-    char operation = match.str(1)[0];
-    return operation;
+    regex_search(cell, match, oper);
+    FormulaCell parsedFormula(
+        std::make_pair(matches[0].str(1), matches[0].str(2)),
+        std::make_pair(matches[1].str(1), matches[1].str(2)), match.str(1)[0]);
+
+    return parsedFormula;
 }
 
 bool isNumber(std::string cell) {
@@ -63,90 +66,115 @@ bool isWord(std::string cell) {
     return std::regex_match(cell, r);
 }
 
-void calculate(std::vector < std::vector < std::string >> & tableData) {
+std::pair<int, int> indicesCell(
+    std::pair<std::string, std::string> argument,
+    std::vector<std::vector<std::string>> &tableData) {
+    std::pair<int, int> indices(-1, -1);
+    for (int j = 0; j < tableData[0].size(); j++) {
+        if (argument.first == tableData[0][j]) {
+            indices.second = j;
+            break;
+        }
+    }
+    for (int i = 0; i < tableData.size(); i++) {
+        if (argument.second == tableData[i][0]) {
+            indices.first = i;
+            break;
+        }
+    }
+    return indices;
+}
+
+int calculateExpression(int a, int b, char operation) {
+    int result;
+    switch (operation) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            if (b == 0) {
+                throw "Error: division by zero";
+            }
+            result = a / b;
+            break;
+        default:
+            throw "Error: invalid operation";
+    }
+    return result;
+}
+
+void parseAndCalculateTable(std::vector<std::vector<std::string>> &tableData) {
     for (int i = 0; i < tableData.size(); i++) {
         for (int j = 0; j < tableData[i].size(); j++) {
             std::string tempCell = tableData[i][j];
 
             if (isFormula(tempCell)) {
-                std::vector < std::pair < std::string, std::string >> arguments = parseArguments(tempCell);
-                //auto arguments = parseArguments(tempCell);
-                char operation = parseOperation(tempCell);
+                // std::vector < std::pair < std::string, std::string >>
+                // arguments = parseArguments(tempCell);
+                FormulaCell parsedCell = parseFormula(tempCell);
+                // auto arguments = parseArguments(tempCell);
+                char operation = parsedCell.operation;
                 int k = 1;
                 std::string arg1Letter, arg2Letter, arg1Digit, arg2Digit;
                 int a, b;
-                std::pair < int, int > aIndex(-1, -1);
-                std::pair < int, int > bIndex(-1, -1);
+                std::pair<int, int> aIndex(-1, -1);
+                std::pair<int, int> bIndex(-1, -1);
 
-                for (int i = 0; i < tableData.size(); i++) {
-                    if (arguments[0].second == tableData[i][0]) {
-                        aIndex.first = i;
-                        break;
-                    }
-                }
+                std::stack<FormulaCell> stack;
+                stack.push(parsedCell);
+                while (!stack.empty() &&
+                       stack.size() < tableData[i].size() - 1) {
+                    aIndex = indicesCell(parsedCell.argument1, tableData);
+                    bIndex = indicesCell(parsedCell.argument2, tableData);
 
-                for (int i = 0; i < tableData.size(); i++) {
-                    if (arguments[1].second == tableData[i][0]) {
-                        bIndex.first = i;
-                        break;
+                    if (isFormula(tableData[aIndex.first][aIndex.second])) {
+                        stack.push(parseFormula(
+                            tableData[aIndex.first][aIndex.second]));
+                    } else if (isFormula(tableData[bIndex.first][bIndex.second]
+                                             .c_str())) {
+                        stack.push(parseFormula(
+                            tableData[bIndex.first][bIndex.second]));
+                    } else {
+                        if (aIndex.first * aIndex.second * bIndex.first *
+                                bIndex.second <
+                            0) {
+                            throw "Error: reference to a non-existent cell";
+                        }
+                        a = atoi(
+                            tableData[aIndex.first][aIndex.second].c_str());
+                        b = atoi(
+                            tableData[bIndex.first][bIndex.second].c_str());
+                        tableData[i][j] = std::to_string(
+                            calculateExpression(a, b, operation));
+                        stack.pop();
                     }
                 }
-                for (int j = 0; j < tableData[0].size(); j++) {
-                    if (arguments[0].first == tableData[0][j]) {
-                        aIndex.second = j;
-                        break;
-                    }
-                }
-
-                for (int j = 0; j < tableData[0].size(); j++) {
-                    if (arguments[1].first == tableData[0][j]) {
-                        bIndex.second = j;
-                        break;
-                    }
-                }
-                if (aIndex.first * aIndex.second * bIndex.first * bIndex.second < 0) {
-                    throw "Error: reference to a non-existent cell";
-                }
-                a = atoi(tableData[aIndex.first][aIndex.second].c_str());
-                b = atoi(tableData[bIndex.first][bIndex.second].c_str());
-                switch (operation) {
-                case '+':
-                    tableData[i][j] = std::to_string(a + b);
-                    break;
-                case '-':
-                    tableData[i][j] = std::to_string(a - b);
-                    break;
-                case '*':
-                    tableData[i][j] = std::to_string(a * b);
-                    break;
-                case '/':
-                    if (b == 0) {
-                        throw "Error: division by zero";
-                    }
-                    tableData[i][j] = std::to_string(a / b);
-                    break;
-                default:
-                    throw "Error: invalid operation";
-                }
-            } else if (!isNumber(tempCell) && (!isWord(tempCell) && i == 0) && (!tempCell.empty() && i == 0 && j == 0)) {
-                throw ("Error: invalid cell value");
+            } else if (!isNumber(tempCell) && (!isWord(tempCell) && i == 0) &&
+                       (!tempCell.empty() && i == 0 && j == 0)) {
+                throw("Error: invalid cell value");
             }
         }
     }
 }
 
-
-
 void readAndCalculateCsv(std::string fileName) {
     std::string suffix = ".csv";
-    if (suffix.length() > fileName.length() || fileName.compare(fileName.length() - suffix.length(), suffix.length(), suffix) != 0) {
+    if (suffix.length() > fileName.length() ||
+        fileName.compare(fileName.length() - suffix.length(), suffix.length(),
+                         suffix) != 0) {
         throw "Error: invalid input. The file must be in csv format";
     }
     std::ifstream file(fileName);
-    std::vector < std::vector < std::string >> tableData;
+    std::vector<std::vector<std::string>> tableData;
     if (file.is_open()) {
         std::string line, cell;
-        std::vector < std::string > vectorLine;
+        std::vector<std::string> vectorLine;
         while (std::getline(file, line)) {
             std::stringstream ssLine(line);
             while (std::getline(ssLine, cell, ',')) {
@@ -155,12 +183,11 @@ void readAndCalculateCsv(std::string fileName) {
             tableData.push_back(vectorLine);
             vectorLine.clear();
         }
-
     } else {
         throw "Error: File could not open";
     }
     file.close();
-    calculate(tableData);
+    parseAndCalculateTable(tableData);
     for (int i = 0; i < tableData.size(); i++) {
         for (int j = 0; j < tableData[i].size(); j++) {
             std::cout << tableData[i][j];
@@ -173,17 +200,16 @@ void readAndCalculateCsv(std::string fileName) {
     return;
 }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
     if (argc == 1 || argc > 2) {
         throw "Error: invalid input. The name of the csv file must be passed as the first argument on the command line.";
     }
     std::string fileName = argv[1];
     try {
         readAndCalculateCsv(fileName);
-    } catch (char
-        const * str) {
+    } catch (char const *str) {
         std::cout << str << std::endl;
     }
-    //std::cout << __cplusplus << std::endl;
+    // std::cout << __cplusplus << std::endl;
     return 0;
 }
